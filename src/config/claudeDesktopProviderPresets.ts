e7493b3d6c6ff5b3ba4241eba2171d5d86076682
DOVEGUO <791495482@qq.com>
2026-09-26 06:50:16 +0800

    refactor: remove legacy Command Code desktop type

--- src/config/claudeDesktopProviderPresets.ts
+++ src/config/claudeDesktopProviderPresets.ts
@@ -16,8 +16,7 @@ export type ClaudeDesktopApiFormat =
   | "openai_chat"
   | "openai_responses"
   | "gemini_native"
-  | "commandcode"
-  | "commandcode_go";
+  | "commandcode";
 
 export interface ClaudeDesktopRoutePreset {
   routeId: string;
